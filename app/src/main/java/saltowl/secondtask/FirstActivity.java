1be911aef37d2bc22f959a902806c0f70033cc20
saltowl <vladislava-shakirova@mail.ru>
2018-02-16 16:39:18 +0300

    Create connection with main menu

--- app/src/main/java/saltowl/secondtask/FirstActivity.java
+++ app/src/main/java/saltowl/secondtask/FirstActivity.java
@@ -1,13 +1,36 @@
 package saltowl.secondtask;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class FirstActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
